00aed86e793b79ce0e997c1f66ba80345c40b9c5
physicslabgmu <138739610+physicslabgmu@users.noreply.github.com>
2025-02-26 20:34:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 const mongoose = require('mongoose');
-const authRoutes = require('./auth-routes');
+const authRoutes = require('./auth-route');
 
 const app = express();
 const port = process.env.PORT || 3000;
